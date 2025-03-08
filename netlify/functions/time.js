be4083246b57da9c20816d854ec591ad46964d7c
Jason Juhyun Kim <mtinet@hanmail.net>
2025-03-08 14:59:18 +0900

    Update time.js

--- netlify/functions/time.js
+++ netlify/functions/time.js
@@ -1,9 +1,10 @@
-exports.handler = async () => {
+exports.handler = async (event) => {
   return {
     statusCode: 200,
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*", // CORS 허용
     },
-    body: JSON.stringify({ time: new Date().toISOString() })
+    body: JSON.stringify({ time: new Date().toISOString() }),
   };
 };
